4a4d6e2396c8af1eadf1367d1d43d627a52a254f
Yar Zar Thein Han <yazartheinhan9@gmail.com>
2026-07-06 10:58:29 +0630

    Changed to use lombok

--- src/main/java/com/example/ecommerce/service/CategoryServiceImpl.java
+++ src/main/java/com/example/ecommerce/service/CategoryServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.ecommerce.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -31,28 +30,16 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public String deleteCategory(long categoryId){
-        List<Category> categories = categoryRepository.findAll();
-        Category category = categories.stream()
-        .filter(c -> Long.valueOf(c.getCategoryId()).equals(categoryId))
-        .findFirst().orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category Not Found!"));
-
+        Category category = categoryRepository.findById(categoryId).orElseThrow(
+            ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category you delete was not found!"));
         categoryRepository.delete(category);
         return "Category ID deleted successfully";
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId){
-        List<Category> categories = categoryRepository.findAll();
-        Optional<Category> optionalCategory = categories.stream().filter
-        (c -> c.getCategoryId() == categoryId).findFirst();
-        if(optionalCategory.isPresent()){
-            Category existingCategory = optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            return categoryRepository.save(existingCategory);
-        } else{
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category Not Found To Update");
-        }
-
+        category.setCategoryId(categoryId);
+       return categoryRepository.save(category);
     }
     
 
